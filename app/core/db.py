15a4f40e8f245fdef1f027787b28d0618409a76f
Александр <aleksandr@MacBook-Pro-Aleksandr.local>
2024-02-12 23:50:49 +0300

    refactor yelding connection from pool

--- app/core/db.py
+++ app/core/db.py
@@ -3,6 +3,8 @@ from asyncpg import Pool
 
 from app.core.config import config
 
+connection_pool = None
+
 
 async def create_connection_pool() -> Pool:
     return await asyncpg.create_pool(
@@ -14,6 +16,9 @@ async def create_connection_pool() -> Pool:
 
 
 async def get_db_async_connection():
-    pool = await create_connection_pool()
-    async with pool.acquire() as connection:
+    global connection_pool
+
+    if connection_pool is None:
+        connection_pool = await create_connection_pool()
+    async with connection_pool.acquire() as connection:
         yield connection
